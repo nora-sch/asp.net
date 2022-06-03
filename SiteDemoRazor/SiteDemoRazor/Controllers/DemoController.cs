696f02b40e1f792da59f1aea1c540e41fcac6b0f
nora-sch <nora.schumann.development@gmail.com>
2022-06-03 12:37:06 +0200

    Demo passer message MVC

---
+++ SiteDemoRazor/SiteDemoRazor/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using SiteDemoRazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteDemoRazor.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Ceci est un message porté par le ViewBag";
+            TempData["Message"] = "Ceci est un message porté par le TempData";
+
+            var vm = new MessageVM { Message = "Ceci est un message porté par le ViewModel"};
+            return View(vm);
+        }
+    }
+}
