abf8a47090744b3a4a353cbaa4871d7e92a26a62
nora-sch <nora.schumann.development@gmail.com>
2022-06-03 14:57:54 +0200

    Demo partials

--- SiteDemoRazor/SiteDemoRazor/Controllers/PersonneController.cs
+++ SiteDemoRazor/SiteDemoRazor/Controllers/PersonneController.cs
@@ -8,24 +8,39 @@ using System.Web.Mvc;
 namespace SiteDemoRazor.Controllers
 {
     public class PersonneController : Controller
-    {
-        // GET: Personne
-        public ActionResult Index()
+         {
+        static List<Personne> personnes;
+
+        // constructeur du contrôleur
+        public PersonneController()
         {
-            var personnes = new List<Personne>
+            if(personnes == null)
+            {
+                personnes = new List<Personne>
             {
                 new Personne{Id=1, Age=20, Nom="LUPINE", Prenom="Arthur"},
                 new Personne{Id=2, Age=25, Nom="ROGNE", Prenom="Yves"},
                 new Personne{Id=3, Age=30, Nom="PACCIO", Prenom="Oscar"},
                 new Personne{Id=4, Age=35, Nom="NICOUETTE", Prenom="Sandre"}
             };
+            }
+
+        }
+        // GET: Personne
+        public ActionResult Index()
+        {
             return View(personnes);
         }
 
         // GET: Personne/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var personne = personnes.FirstOrDefault(p => p.Id == id);
+            if (personne != null)
+            {
+                return View(personne);
+            }
+            return RedirectToAction("Index");
         }
 
         // GET: Personne/Create
@@ -75,7 +90,12 @@ namespace SiteDemoRazor.Controllers
         // GET: Personne/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var personne = personnes.FirstOrDefault(p => p.Id == id);
+            if (personne != null)
+            {
+                return View(personne);
+            }
+            return RedirectToAction("Index");
         }
 
         // POST: Personne/Delete/5
@@ -85,6 +105,8 @@ namespace SiteDemoRazor.Controllers
             try
             {
                 // TODO: Add delete logic here
+                var personne = personnes.FirstOrDefault(p => p.Id == id);
+                personnes.Remove(personne);
 
                 return RedirectToAction("Index");
             }
