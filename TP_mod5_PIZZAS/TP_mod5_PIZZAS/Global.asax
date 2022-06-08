891a85036568b085dc2104811759e6ba76a3f6ca
nora-sch <nora.schumann.development@gmail.com>
2022-06-08 11:30:44 +0200

    TP Pizzas first commit

---
+++ TP_mod5_PIZZAS/TP_mod5_PIZZAS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TP_mod5_PIZZAS.MvcApplication" Language="C#" %>
---
+++ TP_mod5_PIZZAS/TP_mod5_PIZZAS/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales sur un assembly sont contrôlées via ce
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("TP_mod5_PIZZAS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TP_mod5_PIZZAS")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L’affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("412c61df-8551-43a8-b099-5495d40ee3d5")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Major Version
+//      Version mineure
+//      Build Number
+//      Revision
+//
+// Vous pouvez spécifier toutes les valeurs ou vous pouvez définir par défaut les valeurs Révision et Numéro de version
+// en utilisant '*', comme indiqué ci-dessous :
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
