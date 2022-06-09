16db7ee1da3a481b3db5222fbc213f0b1fcb6645
nora-sch <nora.schumann.development@gmail.com>
2022-06-09 09:30:24 +0200

    Demo DbContext connexion à la BDD

---
+++ DemoDbContext/DemoDbContext/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDbContext
+{
+    public class Context:DbContext
+    {
+        public DbSet<Personne> Personnes { get; set; }         
+
+    }
+}
---
+++ DemoDbContext/DemoDbContext/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDbContext
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context();
+            foreach(var personne in context.Personnes)
+            {
+                Console.WriteLine($"Nom:{personne.Nom}, Prenom:{personne.Prenom}");
+            }
+            if (!context.Personnes.Any())
+            {
+                context.Personnes.Add(new Personne { Nom = "PACCIO", Prenom = "Oscar" });
+                context.SaveChanges();
+                Console.WriteLine($"Une personne ajouté!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
